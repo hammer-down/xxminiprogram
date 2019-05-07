20127aec4a76e9134c7cbc1be73582109a58be00
DSGDragon <48942442+DSGDragon@users.noreply.github.com>
2019-05-07 21:42:45 +0800

    update add

--- pages/data/dishData.js
+++ pages/data/dishData.js
@@ -1,7 +1,7 @@
 var dishData = [
 {
   "dishid": 0,
-    "dishImgUrl": "https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=03185e533412b31bd361c57be7715d1f/0df431adcbef76093ba35b8b23dda3cc7cd99e7e.jpg",
+  "dishImgUrl": "https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=03185e533412b31bd361c57be7715d1f/0df431adcbef76093ba35b8b23dda3cc7cd99e7e.jpg",
   "dishName": "糖醋排骨",
   "dsihPrice": "10",
   "dishDescription": "糖醋排骨可以说是永远经典的一道菜。无论大人孩子都会对它爱不适口。一般做法都是油炸过再糖醋，我喜欢煮过再糖醋，自己感觉这样不会油腻，口感更好，而且更健康一些。",
